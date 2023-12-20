a8e50925864507028b8a7a7ffaebf2924df04e5f
Daria-Koshkina <64657562+Daria-Koshkina@users.noreply.github.com>
2023-12-21 00:26:41 +0200

    Implented fehwick tree

---
+++ FehwickTree.cpp
@@ -0,0 +1,68 @@
+﻿#include <iostream>
+#include <vector>
+
+class Fehwick_tree {
+public:
+    Fehwick_tree(int n) {
+        this->n = n;
+        tree.assign(n, std::vector<std::vector<int64_t>>(n, std::vector<int64_t>(n, 0)));
+    }
+    int n;
+    std::vector<std::vector<std::vector<int64_t> > > tree;
+
+    int64_t sum(int x, int y, int z) {
+        int result = 0;
+        for (int i = x; i >= 0; i = (i & (i + 1)) - 1) {
+            for (int j = y; j >= 0; j = (j & (j + 1)) - 1) {
+                for (int k = z; k >= 0; k = (k & (k + 1)) - 1) {
+                    result += tree[i][j][k];
+                }
+            }
+        }
+        return result;
+    }
+
+    void modify(int x, int y, int z, int64_t delta) {
+        for (int i = x; i < n; i = i | (i + 1)) {
+            for (int j = y; j < n; j = j | (j + 1)) {
+                for (int k = z; k < n; k = k | (k + 1)) {
+                    tree[i][j][k] += delta;
+                }
+            }
+        }
+    }
+};
+
+int main()
+{
+    int n;
+    std::cin >> n;
+    Fehwick_tree fenwick_tree = Fehwick_tree(n);
+    std::vector<int64_t> results(0);
+    bool flag = true;
+    while (flag) {
+        int sign, x, y, z, d, x1, y1, z1, x2, y2, z2;
+        std::cin >> sign;
+        if (sign == 1) {
+            std::cin >> x >> y >> z >> d;
+            fenwick_tree.modify(x, y, z, d);
+        }
+        else if (sign == 2) {
+            std::cin >> x1 >> y1 >> z1 >> x2 >> y2 >> z2;
+            x1--; y1--; z1--;
+            int64_t result = fenwick_tree.sum(x2, y2, z2) - fenwick_tree.sum(x1, y1, z1) -
+                fenwick_tree.sum(x1, y2, z2) - fenwick_tree.sum(x2, y1, z2) - fenwick_tree.sum(x2, y2, z1) +
+                fenwick_tree.sum(x2, y1, z1) + fenwick_tree.sum(x1, y2, z1) + fenwick_tree.sum(x1, y1, z2);
+            results.push_back(result);
+        }
+        else {
+            flag = false;
+        }
+    }
+
+    for (int i = 0; i < results.size(); ++i) {
+        std::cout << results[i] << "\n";
+    }
+
+    return 0;
+}
---
+++ Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DijkstraAlgorithm
+{
+    class Flight
+    {
+        public int depertureAirportId;
+        public int arivalAirportId;
+        public DateTime departureDate;
+        public DateTime arrivalDate;
+
+        public Flight(int depertureAirportId, int arivalAirportId, DateTime departureDate, DateTime arrivalDate)
+        {
+            this.depertureAirportId = depertureAirportId;
+            this.arivalAirportId = arivalAirportId;
+            this.departureDate = departureDate;
+            this.arrivalDate = arrivalDate;
+        }
+
+    }
+
+    class ShortPath
+    {
+        public List<List<int>> paths;
+        public List<int> airports;
+        public List<int> depAirports;
+        public List<int> arrAirports;
+        public List<Flight> flights;
+        public DateTime depDate;
+        public int k = 5;
+
+        public ShortPath(List<int> airports, List<Flight> flights, List<int> depAirports, List<int> arrAirports, DateTime depDate)
+        {
+            this.airports = airports;
+            this.depAirports = depAirports;
+            this.arrAirports = arrAirports;
+            this.depDate = depDate;
+            this.flights = flights;
+        }
+
+        public List<List<Flight>> getShortPath()
+        {
+            List<List<Flight>> paths = new List<List<Flight>>();
+            for (int i = 0; i < depAirports.Count; ++i)
+            {
+                for (int j = 0; j < flights.Count; ++j)
+                {
+                    if (flights[j].depertureAirportId == depAirports[i])
+                    {
+                        List<List<Flight>> newPaths = getShortPath(flights[j]);
+                        paths.AddRange(newPaths);
+                    }
+                }
+            }
+            List<List<Flight>> filteredPaths = paths.Where(x => arrAirports.Contains(x.Last().arivalAirportId)).ToList();
+            filteredPaths.OrderBy(x => x.Last().arrivalDate);
+            return filteredPaths;
+        }
+
+        List<List<Flight>> getShortPath(Flight flight)
+        {
+            List<List<Flight>> paths = new List<List<Flight>>();
+            Flight currFlight = flight;
+            if (arrAirports.Contains(currFlight.arivalAirportId))
+            {
+                List<Flight> path = new List<Flight>() { currFlight };
+                paths.Insert(0, path);
+                return paths;
+            }
+            for (int i = 0; i < flights.Count; ++i)
+            {
+                if(flights[i].depertureAirportId == currFlight.arivalAirportId && flights[i].departureDate > currFlight.arrivalDate)
+                {
+                    List<List<Flight>> nextFlights = getShortPath(flights[i]);
+                    paths.AddRange(nextFlights);
+                }
+            }
+            for (int i = 0; i < paths.Count; ++i)
+            {
+                paths[i].Insert(0, currFlight);
+            }
+            if (paths.Count == 0)
+            {
+                List<Flight> path = new List<Flight>() { currFlight };
+                paths.Insert(0, path);
+            }
+            return paths;
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Flight flight1 = new Flight(1, 2, DateTime.Parse("Jan 1, 2009 03:00"), DateTime.Parse("Jan 1, 2009 10:00"));
+            Flight flight2 = new Flight(1, 3, DateTime.Parse("Jan 1, 2009 05:00"), DateTime.Parse("Jan 1, 2009 17:00"));
+            Flight flight3 = new Flight(2, 3, DateTime.Parse("Jan 1, 2009 11:00"), DateTime.Parse("Jan 1, 2009 13:00"));
+            Flight flight4 = new Flight(2, 3, DateTime.Parse("Jan 1, 2009 01:00"), DateTime.Parse("Jan 1, 2009 20:30"));
+            Flight flight5 = new Flight(3, 2, DateTime.Parse("Jan 2, 2009 07:00"), DateTime.Parse("Jan 2, 2009 10:00"));
+
+            List<int> dep = new List<int>() { 1 };
+            List<int> arr = new List<int>() { 2 };
+            List<Flight> flights = new List<Flight>();
+            flights.Add(flight1);
+            flights.Add(flight2);
+            flights.Add(flight3);
+            flights.Add(flight4);
+            flights.Add(flight5);
+
+            var newDate = DateTime.Parse("Jan 1, 2009 00:00");
+            List<int> airports = new List<int>() { 1, 2, 3 };
+
+            var algorithm = new ShortPath(airports, flights, dep, arr, newDate);
+            var paths = algorithm.getShortPath();
+            foreach(List<Flight> path in paths)
+            {
+                foreach (Flight flight in path)
+                {
+                    Console.WriteLine(flight.depertureAirportId);
+                    Console.WriteLine(flight.arivalAirportId);
+                    Console.WriteLine(flight.departureDate);
+                    Console.WriteLine("--------------");
+
+
+                }
+                Console.WriteLine("---------------------------------------");
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
